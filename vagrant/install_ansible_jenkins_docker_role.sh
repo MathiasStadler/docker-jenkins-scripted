f2c87a976dfcb20afbf1188d5e71d4ba9d32ea6d
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-06-29 11:44:21 +0200

    add config.vm.hostname = VM_NAME

--- vagrant/install_ansible_jenkins_docker_role.sh
+++ vagrant/install_ansible_jenkins_docker_role.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 set -e
+
+# clone repo
+git clone https://github.com/geerlingguy/ansible-role-jenkins.git
+
+# change into directory
+cd ansible-role-jenkins
+
+# check syntax is compatible with ansible version
+ansible-playbook --syntax-check playbook.yml
+
+# install requirements
+ansible-galaxy install -r requirements.yml
+
+# run playbook
+ansible-playbook playbook.yml
