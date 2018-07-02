7bf6a60f4f2d67ecb0fe97efd7ae10963fc3137e
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-07-02 20:03:39 +0200

    fix "" inside script

--- vagrant/found-bridge-adapter.sh
+++ vagrant/found-bridge-adapter.sh
@@ -53,11 +53,11 @@ mask2cdr() {
 cdr2mask() {
 	# Number of args to shift, 255..255, first non-255 byte, zeroes
 	set -- $((5 - ($1 / 8))) 255 255 255 255 $(((255 << (8 - ($1 % 8))) & 255)) 0 0 0
-	[ $1 -gt 1 ] && shift $1 || shift
-	echo ${1-0}.${2-0}.${3-0}.${4-0}
+	[ "$1" -gt 1 ] && shift "$1" || shift
+	echo "${1-0}.${2-0}.${3-0}.${4-0}"
 }
 
-readonly NETMASK=cdr2mask $CIDR
+readonly NETMASK=$(cdr2mask "${CIDR}")
 
 echo "First bridge interface of vm"
 echo "Adapter ${ADAPTER}"
