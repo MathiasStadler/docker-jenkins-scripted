12fcbb632970014ccab566bd77277ebb94e4065e
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-07-12 09:20:57 +0200

    add rm apache-maven link

--- install-maven-debian.sh
+++ install-maven-debian.sh
@@ -5,8 +5,9 @@ set -e
 MAVEN_FILE="apache-maven-3.5.4-bin.tar.gz"
 PROFILE_D_FILE="/etc/profile.d/apache-maven.sh"
 cd /usr/local || exit 1
-sudo curl http://www-eu.apache.org/dist/maven/maven-3/3.5.4/binaries/${MAVEN_FILE} -o /tmp/${MAVEN_FILE}
+curl http://www-eu.apache.org/dist/maven/maven-3/3.5.4/binaries/${MAVEN_FILE} -o /tmp/${MAVEN_FILE}
 sudo tar -xzf /tmp/${MAVEN_FILE} -C /opt
+sudo rm -rf apache-maven
 sudo ln -f -s ${MAVEN_FILE} apache-maven
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" | sudo tee -a ${PROFILE_D_FILE} >/dev/null
 echo "export M2_HOME=/usr/local/apache-maven" | sudo tee -a ${PROFILE_D_FILE} >/dev/null
